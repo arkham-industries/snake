fcdf4982a9daf6d571746b550d71fc860e3f2dec
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2022-01-09 20:27:05 -0800

    draws the snake initially

--- src/client/app.js
+++ src/client/app.js
@@ -11,9 +11,9 @@ export class App {
         const worldHeight = 20;
         const worldWidth = 20;
 
-        const grid = new Grid(this.appEl, worldHeight, worldWidth);
-        console.log('game started!', grid);
-        grid.render();
+        this.grid = new Grid(this.appEl, worldHeight, worldWidth);
+        console.log('game started!', this.grid);
+        this.grid.render();
 
         const snakeBodySegments = this.getInitialSnakeBodySegements(worldHeight, worldWidth);
         this.snake = new Snake(snakeBodySegments);
@@ -29,7 +29,7 @@ export class App {
      */
     update() {
         console.log('updating the game!', this.snake);
-        // this.snake
+        this.grid.colorCells(this.snake.bodySegments, '#f00');
     }
 
     getInitialSnakeBodySegements(worldHeight, worldWidth){ //spawns a length 2 snake at random point
@@ -38,14 +38,14 @@ export class App {
         const bodySegments = [];
         bodySegments.push([spawnX,spawnY]);
       
-        if(Math.round(Math.random)){
-            if(Math.round(Math.random)){ //if 0 use X
+        if(Math.round(Math.random())){
+            if(Math.round(Math.random())){ //if 0 use X
                 bodySegments.push([spawnX + 1, spawnY]); //+1 to X
             } else {
                 bodySegments.push([spawnX - 1, spawnY]);//-1 to X
             }
         } else {
-            if(Math.round(Math.random)){ //if 0 use Y
+            if(Math.round(Math.random())){ //if 0 use Y
                 bodySegments.push([spawnX, spawnY + 1]); //+1 to Y
             } else {
                 bodySegments.push([spawnX, spawnY -1]);//-1 to Y
--- src/client/grid.js
+++ src/client/grid.js
@@ -7,17 +7,30 @@ export class Grid {
     }
 
     render() {
-        const table = document.createElement('table');
-        table.classList.add("game-grid");
+        this.table = document.createElement('table');
+        this.table.classList.add("game-grid");
         for(let y=0; y<this.height; y++) {
             const row = document.createElement('tr');
-            table.append(row);
+            this.table.append(row);
             for(let x=0; x<this.width; x++) {
                 const cell = document.createElement('td');
                 row.append(cell);
             }
         }
-        this.appEl.append(table);
+        this.appEl.append(this.table);
+    }
+
+    /**
+     * This method colors the cells in a specified color value.
+     * @param {*} cellArray an array of tuples, e.g. [[1,2],[0,2]]
+     * @param {*} color is a rgb color, e.g. #f00
+     */
+    colorCells(cellArray, color) {
+        cellArray.forEach((cellCoordinates) => {
+            const tableRow = this.table.children.item(cellCoordinates[0]);
+            const tableCell = tableRow.children.item(cellCoordinates[1]);
+            tableCell.style.backgroundColor = color;
+        });
     }
 }
 
